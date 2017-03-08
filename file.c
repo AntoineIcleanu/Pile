8081bb8a41b52bf6ba823e3ccb853af6b38903b0
AntoineIcleanu <a.icleanu@laposte.net>
2017-03-08 10:42:15 +0100

    Ajout de file

--- file.c
+++ file.c
@@ -10,5 +10,34 @@ struct carre{
 typedef struct file file;
 struct file{
     carre *premier;
+};
+
+void enfiler(file *file, int nvNombre){
+    carre *nouveau;
+    if(file != NULL || nouveau != NULL){
+        nouveau->nombre = nvNombre;
+        nouveau->suivant = NULL;
+    }
+    if (file->premier != NULL){
+        carre *carreActuel = file->premier;
+        while (carreActuel->suivant != NULL){
+            carreActuel = carreActuel->suivant;
+        }
+        carreActuel->suivant = nouveau;
+    }
+    else{
+        file->premier = nouveau;
+    }
 }
 
+void defiler(file *file){
+    if (file != NULL){
+        int nombredefile = 0;
+        if (file->premier != NULL){
+        carre *carredefile = file->premier;
+        nombredefile = carredefile->nombre;
+        file->premier = carredefile->suivant;
+        free(carredefile);
+        }
+    }
+}
